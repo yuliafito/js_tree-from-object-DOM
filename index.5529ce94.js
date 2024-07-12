41856401b7e5db3cc79af351395d63ad0a2ae085
Yuliia Fito <jylia.fito2004@gmail.com>
2024-07-12 09:31:30 +0300

    make build

---
+++ index.5529ce94.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length>0&&e(l,t[a])}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.5529ce94.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    for(const key in data){
+        const li = document.createElement("li");
+        li.textContent = key;
+        ul.appendChild(li);
+        if (Object.keys(data[key]).length > 0) createTree(li, data[key]);
+    }
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
